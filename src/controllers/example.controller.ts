b55a8fd22cb5ed0b2e5d3d05b90b899ecc01f9d2
AdrianHernandezIslas <15161315@itoaxaca.edu.mx>
2022-10-19 17:55:36 -0500

    Refactor(Example):Update label response ok

--- src/controllers/example.controller.ts
+++ src/controllers/example.controller.ts
@@ -4,5 +4,5 @@ import {ExampleModel} from "../models/example.model";
 export async function createExample(req: Request, res: Response): Promise<Response> {
     const {params:{idStatus,status,description}} = req;
     await ExampleModel.create({idStatus:Number(idStatus),status,description});
-    return res.status(201).send("Creado");
+    return res.status(201).send("Creado con ajuste");
   }
