154002d5943de161f10b5113ac94812e56db792e
Jared-Pinacho <osmar35pinacho@gmail.com>
2022-11-15 21:35:10 -0600

    aceeso a bd sin errores

--- src/controllers/loggin.controller.ts
+++ src/controllers/loggin.controller.ts
@@ -3,5 +3,5 @@ import { Request, Response } from "express";
 export function renderLoggin(req:Request,res:Response){
     const data = {valorDeEjemplo:"Vengo desde el controlador",
     numeroEjemplo:101};
-    res.render("login",data);
+    res.render("table-component",data);
 }
--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -1,19 +1,19 @@
 import { Sequelize } from "sequelize";
 
-const DB_USER = process.env.DB_USER as string;
-const DB_PASSWORD = process.env.DB_PASSWORD as string;
-const DB_NAME = process.env.DB_NAME as string;
-const DB_HOST = process.env.DB_HOST as string;
+const DB_USER = "postgres"
+const DB_PASSWORD = "queen"
+const DB_NAME = "postgres"
+const DB_HOST = "localhost"
 const DB_DIALECT = "postgres";
 
 export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   dialect: DB_DIALECT,
+
   dialectOptions: {
-    encrypt: true,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    encrypt: true
+   
   },
   logging:false
 });
+
