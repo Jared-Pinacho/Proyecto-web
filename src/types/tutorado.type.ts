9bed790560ed8d83f49c21412a5ea52450a0bd35
Jared-Pinacho <osmar35pinacho@gmail.com>
2022-11-17 19:29:02 -0600

    funciona cambiar entre vista

---
+++ src/types/tutorado.type.ts
@@ -0,0 +1,10 @@
+export default interface TutoradoType {
+    idTutorado?: number;
+    nombre?: string;
+   edad?: number;
+    sexo?: boolean;
+   aciertos?:string;
+    nivel?:string;
+    idTutor?: number;
+    status?: boolean;
+  }
