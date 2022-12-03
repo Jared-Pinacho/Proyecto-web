11efb7b7abb9486d3d31757f4c0eb338b2923808
JoseRamirezCaballero <19161388@itoaxaca.edu.mx>
2022-12-02 20:59:09 -0600

    configure dotenv

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,6 +1,12 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize('inglesdb', 'postgres', 'admin',{
-    host: 'localhost',
-    dialect: 'postgres'
+const DB_PASSWORD = process.env.DBPASSWORD as string;
+const DB_NAME = process.env.DBNAME as string;
+const DB_HOST = process.env.DBHOST as string;
+const DB_USER = process.env.DB as string;
+const DB_DIALECT = 'postgres';
+
+export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD,{
+    host: DB_HOST,
+    dialect: DB_DIALECT
 })
